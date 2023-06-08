4c183f0068d45f9d8c25284c89acb420ccab6acb
the-aleksandar <aleksandar@cloudteh.com>
2023-06-08 13:36:17 +0200

    Adding deployment codes

---
+++ deploy/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# entrypoint.sh
+
+cat > /etc/crashdragon/config.toml <<EOF
+[db]
+connection = "host=${db_host} user=${db_user} dbname=${db_name} password=${db_password} sslmode=${db_sslmode}"
+[directory]
+assets = "/opt/crashdragon/share/crashdragon/assets"
+content = "/opt/crashdragon/share/crashdragon/files"
+templates = "/opt/crashdragon/share/crashdragon/templates"
+[housekeeping]
+reportretentiontime = "2190h"
+[symbolicator]
+executable = "./minidump_stackwalk"
+trimmodulenames = true
+[web]
+bindaddress = ":8080"
+bindsocket = "/var/run/crashdragon/crashdragon.sock"
+usesocket = false
+EOF
+
+# Start application
+exec "$@"
